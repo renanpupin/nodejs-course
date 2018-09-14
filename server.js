4bf8bc71cc5d79693617d81388e5a6764274b9c7
Renan Pupin <renan.pupin@gmail.com>
2018-09-14 13:43:32 -0300

    improve code

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ mongoose.connect("mongodb://admin:node123456@ds157762.mlab.com:57762/nodejs-cour
 
 let ToDo = require("./src/models/todo");
 
+let todoController = require("../helpers/mongoose")(ToDo);
+const asyncMiddleware = require("../utils/asyncMiddleware");
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -22,9 +25,31 @@ app.get('/todo', function (req, res) {
   	})
 });
 
+app.put('/todo/:id', asyncMiddleware(async(req, res) => {
+    let todo = await todoController.findOne({_id: req.params.id});
+    console.log(todo);
+
+    todo.is_complete = req.body.is_complete;
+    todo.completed_at = new Date();
+
+    let updatedTodo = await todoController.update(todo);
+    console.log("updatedTodo", updatedTodo);
+
+    res.json({ success: true, message: "Successo!!!" });
+}));
+
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!');
 });
 
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	
+	res.json({
+	  success: false,
+	  message: err.message,
+	  error: err
+	});
+});
 
 module.exports = app;
---
+++ src/helpers/asyncMiddleware.js
@@ -0,0 +1,6 @@
+module.exports = asyncMiddleware = fn =>
+    (req, res, next) => {
+        Promise
+            .resolve(fn(req, res, next))
+            .catch(next);
+    };
